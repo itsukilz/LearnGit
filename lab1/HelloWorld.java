4945bb8a0428be9605b660cfd9e1f703e0ec2e11
Lu <tomomedific@gmail.com>
2019-01-27 15:17:37 +0800

    completed first part of lab1

---
+++ lab1/HelloWorld.java
@@ -0,0 +1,19 @@
+// public class HelloWorld{
+// 	public static void main(String[] args){
+// 		System.out.println("hello world");
+// 	}
+// }
+
+
+// 1. filename == class name, capitalized
+// 2. "public static void main(String[] args)" is needed
+// 3. ";" is needed
+
+}
+// SublimeLinter
+
+public class HelloWorld {
+	public static void main(String[] args){
+		System.out.println(5+"10");
+	}
+}
